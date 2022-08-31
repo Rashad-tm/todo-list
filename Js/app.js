c98e546f1ef41dffab4e7f73667f2516510f013b
Mr.BoB <reg_rashad@yahoo.com>
2022-08-31 20:50:27 +0430

    app ready to use, small changes needed!

---
+++ Js/app.js
@@ -0,0 +1,75 @@
+// Extracting Needed Elements From DOM
+
+const userInput = document.querySelector(".user-input");
+const userText = document.querySelector(".user-text");
+const addItemBtn = document.querySelector(".add-item");
+const listArea = document.querySelector(".list-area");
+const removeItemBtn = document.querySelectorAll(".remove-item");
+const changeStatusBtn = document.querySelectorAll(".change-status");
+
+// Global Variables
+const msgBox = document.createElement("div");
+// EventListeners
+addItemBtn.addEventListener("click", addUserInput);
+userText.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        addUserInput();
+    }
+});
+
+listArea.addEventListener("click", removeItemFromList);
+
+// Functions
+
+function addUserInput() {
+    //userText.value === "" ? showErrorMsg : "";
+    if (!userText.value == "") {
+        const createListItem = document.createElement("div");
+        createListItem.classList.add("list-item");
+        createListItem.innerHTML = `
+        <div class="change-status">
+                    <i class="fa-solid fa-circle-dot"></i>
+                </div>
+                <p class="list-text">${userText.value}</p>
+                    <i class="fa-solid fa-trash"></i>
+        </div>
+        `;
+        listArea.appendChild(createListItem);
+        userText.value = "";
+    } else showErrorMsg();
+}
+
+function removeItemFromList(e) {
+    if (e.target.classList.contains("fa-trash")) {
+        e.target.parentElement.remove();
+    }
+}
+
+function changeItemStatus() {}
+
+//! این قسمت کد باید تبدیل شود تا تمیز تر بشود
+//? الان حال ندارم
+// ----
+
+function showErrorMsg() {
+    createMsg();
+    setTimeout(removeMsg, 2000);
+}
+
+function createMsg() {
+    msgBox.innerHTML = `
+        <p style= "
+        color:red;
+        text-align:center;
+        font-size:.7em;
+        ">
+        لطفا متن تسک را وارد کنید!
+        </p>
+    `;
+    listArea.appendChild(msgBox);
+}
+
+function removeMsg() {
+    msgBox.remove();
+}
